7e3fce8dcb9aa367503b13001d54cabd0021da31
T-Damer <dpancov64@gmail.com>
2021-09-01 10:58:22 +0300

    Added task creation

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const TodoList = artifacts.require('./TodoList.sol')
 
 contract('TodoList', (accounts) => {
@@ -19,5 +21,17 @@ contract('TodoList', (accounts) => {
     assert.equal(task.id.toNumber(), taskCount.toNumber())
     assert.equal(task.completed, false)
     assert.equal(task.id.toNumber(), 1)
+  })
+
+  it('creates tasks', async () => {
+    const result = await this.todoList.createTask(
+      'A new task'
+    )
+    const taskCount = await this.todoList.taskCount()
+    assert.equal(taskCount, 2)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 2)
+    assert.equal(event.content, 'A new task')
+    assert.equal(event.completed, false)
   })
 })
